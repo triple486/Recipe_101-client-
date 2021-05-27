40ae57678096b56551c2660313a7031f09edd01e
karuiner <intaekgong@gmail.com>
2021-05-27 18:44:24 +0900

    [client] feature/ 검색 결과 박스만들기

--- recipe101/src/components/Searchresult.tsx
+++ recipe101/src/components/Searchresult.tsx
@@ -6,18 +6,54 @@ import { RootState } from "../redux/reducers";
 const Frame = styled.div`
   flex: 1 0 0;
   border: solid 1px blue;
+  display: flex;
+  flex-direction: column;
 `;
+const Box = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex: 1 0 0;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const TextBox = styled.div`
+  display: flex;
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+function Result({ isFail, search }: { isFail: boolean; search: Object[] }) {
+  if (isFail) {
+    return (
+      <Box>
+        <TextBox>{"검색어를 입력 해 주세요."}</TextBox>
+      </Box>
+    );
+  } else if (search.length) {
+    return (
+      <div>
+        {search.map((x, i) => {
+          return <Recipecard key={i} data={x}></Recipecard>;
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <Box>
+        <TextBox>{"검색된 결과가 없습니다."}</TextBox>
+      </Box>
+    );
+  }
+}
 
 function Searchresult() {
   let data = useSelector((state: RootState) => state.searchReducer);
 
   return (
     <Frame>
-      {data.isFail
-        ? "잘못된 검색을 시행하였습니다."
-        : data.search.length
-        ? `${data.search}`
-        : "검색된 결과가 없습니다."}
+      <Result isFail={data.isFail} search={data.search}></Result>
     </Frame>
   );
 }
