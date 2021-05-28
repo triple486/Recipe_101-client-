2dff06272732d117d891dae5517ba67550c2028d
karuiner <intaekgong@gmail.com>
2021-05-28 12:13:46 +0900

    [client] feature/ 분기

--- recipe101/src/App.tsx
+++ recipe101/src/App.tsx
@@ -39,6 +39,7 @@ function App() {
         <Route exact path="/mypage">
           <Mypage></Mypage>
         </Route>
+
         <Route path="/">
           <Landingpage></Landingpage>
         </Route>
