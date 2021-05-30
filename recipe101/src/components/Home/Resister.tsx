ee0605820d5c8e3d3a8917c8e6bf9a8e07a141c6
karuiner <intaekgong@gmail.com>
2021-05-30 19:31:47 +0900

    [client] feature/ 회원가입 부분 수정

--- recipe101/src/components/Home/Resister.tsx
+++ recipe101/src/components/Home/Resister.tsx
@@ -5,13 +5,45 @@ import { isOn } from "../../redux/modalReducer";
 import CancelButton from "../CancelButton";
 import { useDispatch } from "react-redux";
 import Input from "../Input";
+import axios from "axios";
+import Imageupload from "../ImageUpload";
+import { storeToken } from "../../redux/tokenReducer";
+
+const Modal = styled.div`
+  height: 100vh;
+  width: 100vw;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Frame = styled.div`
-  height: 360px;
-  width: 400px;
+  height: 380px;
+  width: 760px;
   display: flex;
   border: solid 1px red;
   flex-direction: column;
   background: white;
+`;
+const InnerFrame = styled.div`
+  height: 360px;
+  width: 760px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const InputFrame = styled.div`
+  flex: 1 0 0;
+  display: flex;
+  flex-direction: column;
+`;
+const ImageFrame = styled.div`
+  height: 360px;
+  width: 360px;
 `;
 
 const Line = styled.div<{ err?: boolean }>`
@@ -28,13 +60,46 @@ const Button = styled.button`
   height: 100%;
 `;
 
+const IMG = styled.img`
+  height: 340px;
+  width: 360px;
+`;
+
+const IMGBox = styled.div`
+  height: 360px;
+  width: 360px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Imagecancel = styled.button`
+  height: 20px;
+  width: 100%;
+`;
+
+interface imgdata {
+  file?: File;
+  imgpath?: string | ArrayBuffer | null;
+  isin?: boolean;
+}
+
+interface useinfo {
+  username: string;
+  password: string;
+  email: string;
+  phone: string;
+  userImage: imgdata;
+}
 export default function Resister() {
   let location = useLocation();
   let path = location.pathname.slice(0, -9);
-  const [name, namef] = useState("");
-  const [password, passwordf] = useState("");
-  const [email, emailf] = useState("");
-  const [phone, phonef] = useState("");
+  const [userinfo, setuserinfo] = useState<useinfo>({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    userImage: { isin: false },
+  });
   const [validcheck, validcheckf] = useState({
     username: false,
     password: false,
@@ -47,7 +112,7 @@ export default function Resister() {
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
     const username = new RegExp(/^[A-za-z0-9]{5,15}/g);
-    const password = new RegExp(/^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/);
+    const password = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
     const phone = new RegExp(/^\d{2,3}-\d{3,4}-\d{4}$/);
     return function (value: string) {
       let ans = true,
@@ -55,36 +120,45 @@ export default function Resister() {
       switch (type) {
         case "email":
           ans = value.length ? !email.test(value) : false;
-          console.log(ans, value);
           ndata["email"] = ans;
           validcheckf({ ...ndata });
+          if (!ans) {
+            setuserinfo({ ...userinfo, email: value });
+          }
           return ans;
 
         case "password":
           ans = value.length ? !password.test(value) : false;
-          console.log(ans, value);
           ndata["password"] = ans;
           validcheckf({ ...ndata });
+          if (!ans) {
+            setuserinfo({ ...userinfo, password: value });
+          }
+
           return ans;
 
         case "phone":
           ans = value.length ? !phone.test(value) : false;
-          console.log(ans, value);
           ndata["phone"] = ans;
           validcheckf({ ...ndata });
+          if (!ans) {
+            setuserinfo({ ...userinfo, phone: value });
+          }
+
           return ans;
 
         default:
           ans = value.length ? !username.test(value) : false;
-          console.log(ans, value);
           ndata["username"] = ans;
           validcheckf({ ...ndata });
+          if (!ans) {
+            setuserinfo({ ...userinfo, username: value });
+          }
+
           return ans;
       }
-      return;
     };
   }
-
   let history = useHistory();
   let dispatch = useDispatch();
   return (
@@ -95,53 +169,102 @@ export default function Resister() {
           history.push(path.length ? path : "/");
         }}
       ></CancelButton>
-      <Line err={validcheck["username"]}>
-        <Input
-          label={"username"}
-          type={"text"}
-          value={name}
-          func={namef}
-          bfunc={validation("username")}
-        ></Input>
-      </Line>
-      <Line err={validcheck["password"]}>
-        <Input
-          label={"password"}
-          type={"password"}
-          value={password}
-          func={passwordf}
-          bfunc={validation("password")}
-        ></Input>
-      </Line>
-      <Line err={validcheck["email"]}>
-        <Input
-          label={"email"}
-          type={"email"}
-          value={email}
-          func={emailf}
-          bfunc={validation("email")}
-        ></Input>
-      </Line>
-      <Line err={validcheck["phone"]}>
-        <Input
-          label={"phone"}
-          type={"text"}
-          value={phone}
-          func={phonef}
-          bfunc={validation("phone")}
-        ></Input>
-      </Line>
-      <Line></Line>
-      <Line>
-        <Button
-          onClick={() => {
-            history.push(path.length ? path + "/login" : "/login");
-          }}
-        >
-          로그인 창으로
-        </Button>
-        <Button onClick={() => {}}>회원 가입</Button>
-      </Line>
+      <InnerFrame>
+        <ImageFrame>
+          {userinfo.userImage.isin ? (
+            <IMGBox>
+              <IMG
+                src={
+                  typeof userinfo.userImage.imgpath === "string"
+                    ? userinfo.userImage.imgpath
+                    : ""
+                }
+              ></IMG>
+              <Imagecancel
+                onClick={() => {
+                  setuserinfo({
+                    ...userinfo,
+                    userImage: {
+                      isin: false,
+                    },
+                  });
+                }}
+              >
+                {"취소"}
+              </Imagecancel>
+            </IMGBox>
+          ) : (
+            <Imageupload
+              func={(files: any) => {
+                setuserinfo({
+                  ...userinfo,
+                  userImage: {
+                    file: files[0],
+                    imgpath: URL.createObjectURL(files[0]),
+                    isin: true,
+                  },
+                });
+              }}
+              name={"resister2"}
+            />
+          )}
+        </ImageFrame>
+        <InputFrame>
+          <Line err={validcheck["username"]}>
+            <Input label={"username"} bfunc={validation("username")}></Input>
+          </Line>
+          <Line err={validcheck["password"]}>
+            <Input label={"password"} bfunc={validation("password")}></Input>
+          </Line>
+          <Line err={validcheck["email"]}>
+            <Input label={"email"} bfunc={validation("email")}></Input>
+          </Line>
+          <Line err={validcheck["phone"]}>
+            <Input label={"phone"} bfunc={validation("phone")}></Input>
+          </Line>
+          <Line></Line>
+          <Line>
+            <Button
+              onClick={() => {
+                history.push(path.length ? path + "/login" : "/login");
+              }}
+            >
+              로그인 창으로
+            </Button>
+            <Button
+              onClick={() => {
+                let rdata = new FormData();
+                if (userinfo.userImage.file) {
+                  rdata.append("userImage", userinfo.userImage.file);
+                }
+                rdata.append("username", userinfo.username);
+                rdata.append("password", userinfo.password);
+                rdata.append("email", userinfo.email);
+                rdata.append("phone", userinfo.phone);
+                const config = {
+                  headers: {
+                    "content-type": "multipart/form-data",
+                  },
+                };
+                axios
+                  .post(
+                    process.env.REACT_APP_SERVER_URL + "/signup",
+                    rdata,
+                    config
+                  )
+                  .then((res) => {
+                    dispatch(storeToken(res.data.data.accessToken));
+                  })
+                  .catch((err) => {
+                    console.log("fail");
+                  });
+              }}
+            >
+              회원 가입
+            </Button>
+          </Line>
+        </InputFrame>
+      </InnerFrame>
     </Frame>
   );
 }
